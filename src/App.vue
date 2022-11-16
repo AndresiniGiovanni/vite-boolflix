afa138d45f40e97de3ac5929afc3e7597e46c05f
AndresiniGiovanni <gio.andresini@hotmail.it>
2022-11-16 16:05:51 +0100

    Start

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <HeaderComponent />
+    <MainComponent />
+    <CardsComponent />
+  </section>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+import CardsComponent from "./components/CardsComponent.vue";
+
+export default {
+  components: { HeaderComponent, MainComponent, CardsComponent },
+};
+</script>
+
+<style lang="scss" scoped></style>
